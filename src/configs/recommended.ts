2505b0fada8260bd0a4829cbff2605e26f202f99
capitnflam <capitnflam@gmail.com>
2024-03-16 18:01:01 +0100

    fix: typescript related configs only for ts files (#21)

--- src/configs/recommended.ts
+++ src/configs/recommended.ts
@@ -1,28 +1,17 @@
 import { type ESLintConfig } from '../types'
 
 export const recommended: ESLintConfig = {
-  plugins: ['@flaminc', '@typescript-eslint'],
+  plugins: ['@flaminc'],
   extends: [
     'eslint:recommended',
     'plugin:@eslint-community/eslint-comments/recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:import/recommended',
-    'plugin:import/typescript',
     'plugin:unicorn/recommended',
     'plugin:sonarjs/recommended',
     'plugin:security/recommended-legacy',
     'prettier',
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: true,
-  },
-  settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
-  },
+
   rules: {
     'sort-imports': [
       'error',
@@ -55,4 +44,24 @@ export const recommended: ESLintConfig = {
       },
     ],
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      plugins: ['@typescript-eslint'],
+      extends: [
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
+        'plugin:import/typescript',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: true,
+      },
+      settings: {
+        'import/parsers': {
+          '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+      },
+    },
+  ],
 }
