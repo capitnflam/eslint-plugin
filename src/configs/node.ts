e4b8cffdfef0f07fe736aba5cfed4b4857eb6f37
capitnflam <capitnflam@gmail.com>
2024-03-17 21:28:38 +0100

    fix: disable n/no-missing-import on ts files (#25)

--- src/configs/node.ts
+++ src/configs/node.ts
@@ -3,4 +3,12 @@ import { type ESLintConfig } from '../types'
 export const node: ESLintConfig = {
   plugins: ['@flaminc'],
   extends: ['plugin:n/recommended'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'n/no-missing-import': 'off',
+      },
+    },
+  ],
 }
